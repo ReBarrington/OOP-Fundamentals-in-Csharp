d98ad4ec4f36ab04d3639629c152770a6d45a635
unknown <reagan@barringtonfamily.net>
2021-03-27 08:26:08 -0400

    customer class

--- ACM/ACM/Customer.cs
+++ ACM/ACM/Customer.cs
@@ -4,6 +4,16 @@ namespace ACM
 {
     public class Customer
     {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName {  get; set; };
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
         private string _lastName;
         public string LastName
         {
